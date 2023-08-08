4e878aebc069c41d473c63bc8b927776c57ccad5
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-08-08 22:17:59 +0300

    base theme

---
+++ app/src/main/java/ru/github/abstract_repo/Theme.kt
@@ -0,0 +1,17 @@
+package ru.github.abstract_repo
+
+import android.app.Activity
+import android.view.View
+
+
+/**
+ * This class contains fields with theme
+ */
+class Theme {
+
+    private val activity: Activity
+    private val view: View
+    private val recyclerView: RecyclerView
+
+
+}
