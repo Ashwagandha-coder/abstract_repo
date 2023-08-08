646dcb7456167a7ffe99ae4ff97b878f19ec904c
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-08-08 22:19:09 +0300

    coroutine and annotation

--- app/src/main/java/ru/github/abstract_repo/Theme.kt
+++ app/src/main/java/ru/github/abstract_repo/Theme.kt
@@ -2,6 +2,7 @@ package ru.github.abstract_repo
 
 import android.app.Activity
 import android.view.View
+import kotlin.coroutines.CoroutineContext
 
 
 /**
@@ -12,6 +13,10 @@ class Theme {
     private val activity: Activity
     private val view: View
     private val recyclerView: RecyclerView
+
+
+    private val coroutine: Coroutine
+    private val annotation: Annotation
 
 
 }
